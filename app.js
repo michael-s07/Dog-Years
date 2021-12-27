2d6e17dd77cc7fe081e51a90352961f8836364b8
michael-s07 <michael.sosu@amalitech.org>
2021-12-27 13:10:25 +0000

    update

---
+++ app.js
@@ -0,0 +1,10 @@
+let myAge = 25;
+//My Age is stored in a variable called myAge
+let earlyYears = 2;
+earlyYears=earlyYears*10.5;
+
+let laterYears = myAge - 2;
+
+laterYears *= 4;
+console.log(earlyYears)
+console.log(laterYears)
